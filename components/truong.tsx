4ddbabcbc25937cf27f7de54b0ee2355faab925c
DESKTOP-6KHCC5V\Ron <truong.pn.62cntt@ntu.edu.vn>
2023-12-10 17:10:50 +0700

    12/10/2023

--- components/truong.tsx
+++ components/truong.tsx
@@ -1,39 +1,42 @@
 import React from "react";
 import Box from "@mui/material/Box";
-import { createTheme, ThemeProvider } from "@mui/system";
-import { makeStyles } from "@mui/styles";
-import { Theme } from "@mui/system";
+import { Input } from "@mui/base/Input";
 
+interface NoiDung {
+  NoiDungChinh: string;
+  NoiDungPhu: string;
+}
 
-const theme = createTheme({
-  palette: {
-    backgroundGradient: {
-      main: "linear-gradient(40deg, #EF2779 0.79%, #7834BE 79.29%)",
-    },
-  },
-});
-
-const useStyles = makeStyles({
-  root: {
-    background: theme.palette.backgroundGradient.main,
-    border: 0,
-    borderRadius: 3,
-    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
-    color: "white",
-    height: 48,
-    padding: "0 30px",
-  },
-});
+const noidung1: NoiDung = {
+  NoiDungChinh: "At your fingertips",
+  NoiDungPhu: "Lightning fast prototyping",
+};
+const noidung2: NoiDung = {
+  NoiDungChinh: "Subscribe to our Newsletter",
+  NoiDungPhu: "Available exclusivery on Figmaland",
+};
 
 const MyComponent1 = () => {
-  const classes = useStyles();
-
   return (
-    <ThemeProvider theme={theme}>
-      <Box>
-        <Box className={classes.root}>this is my page</Box>
+    <>
+      <Box sx={{ bgcolor: "#f0f0f0" }}>
+        <Box sx={{ marginLeft: "632.5px", paddingTop: "80px" }}>
+          {noidung1.NoiDungChinh}
+        </Box>
+        <Box
+          sx={{ marginLeft: "454.5px", fontSize: "46px", marginTop: "7px " }}
+        >
+          {noidung1.NoiDungPhu}
+        </Box>
+        <Box sx={{ marginLeft: "580.5px", marginTop: "69px" }}>
+          {noidung2.NoiDungChinh}
+        </Box>
+        <Box sx={{ marginLeft: "577px", marginTop: "10px" }}>
+          {noidung2.NoiDungPhu}
+        </Box>
+       <Box></Box>
       </Box>
-    </ThemeProvider>
+    </>
   );
 };
 
